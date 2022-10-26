875181e33cc0fe81b5f9ae4548d8e08f87722856
Spong3ibob <leonardomaran17@gmail.com>
2022-10-26 12:10:38 +0200

    jsnack-4

---
+++ jsnak-4/main.js
@@ -0,0 +1,13 @@
+const nomePartecipante = prompt("Inserisci il tuo nome");
+const invitati = Array["luca", "simone", "francesco","andrea"];
+if(nomePartecipante.includes("luca")){
+    console.log("Può entrare");
+}else if(nomePartecipante.includes("simone")){
+    console.log("Può entrare");
+}else if(nomePartecipante.includes("francesco")){
+    console.log("Può entrare");
+}else if(nomePartecipante.includes("andrea")){
+    console.log("Può entrare");
+}else{
+    console.log("Non può entrare")
+}
